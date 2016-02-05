3d045be6f6707ce9bee6cae9545706ee7b83aea0
Carlos Castillo <niquepa@gmail.com>
2016-02-05 08:09:55 -0500

    - listo Racer y RacerInfo (pag 12)

---
+++ app/models/racer.rb
@@ -0,0 +1,19 @@
+class Racer
+  include Mongoid::Document
+
+  Mongo::Logger.logger.level = ::Logger::INFO
+
+  embeds_one :info, as: :parent, class_name: 'RacerInfo', autobuild: true
+
+  delegate :first_name, :first_name=, to: :info
+  delegate :last_name, :last_name=, to: :info
+  delegate :gender, :gender=, to: :info
+  delegate :birth_year, :birth_year=, to: :info
+  delegate :city, :city=, to: :info
+  delegate :state, :state=, to: :info
+  
+  before_create do |racer|
+    racer.info.id = racer.id
+  end
+
+end
---
+++ app/models/racer_info.rb
@@ -0,0 +1,17 @@
+class RacerInfo
+  include Mongoid::Document
+  field :fn, type: String, as: :first_name
+  field :ln, type: String, as: :last_name
+  field :g, type: String, as: :gender
+  field :yr, type: Integer, as: :birth_year
+  field :res, type: Address, as: :residence
+  field :racer_id, as: :_id
+  field :_id, default:->{ racer_id }
+
+  embedded_in :parent, polymorphic: true
+
+  validates :first_name, :last_name, presence: true
+  validates :gender, inclusion: { in: ["M", "F"] } , presence: true
+  validates :birth_year, numericality: {only_integer: true, less_than: Date.today.year} , presence: true
+
+end
