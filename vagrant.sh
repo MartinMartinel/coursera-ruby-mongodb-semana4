f1f1d8859dcd5e781d573e721b7b50645f713810
Carlos Castillo <niquepa@gmail.com>
2016-02-11 16:11:10 -0500

    - Vagrant Inicial

--- vagrant.sh
+++ vagrant.sh
@@ -1,3 +1,7 @@
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 2> /dev/null
+
+echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
 # Make sure we're using the latest version of the repos
 apt-get update
 
@@ -5,7 +9,8 @@ apt-get install -y nodejs 2> /dev/null
 
 apt-get install -y memcached libmemcached-tools 2> /dev/null
 
-apt-get install -y mongodb-clients mongodb-dev mongodb-server 2> /dev/null
+#apt-get install -y mongodb-clients mongodb-dev mongodb-server 2> /dev/null
+sudo apt-get install -y mongodb-org 2> /dev/null
 
 # Make sure we're in the right folder
 cd /vagrant
